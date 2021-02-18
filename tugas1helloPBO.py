f58c82d748956d52992051783554209c4bf81a25
Hafiyyan Putra <hafiyyan.jtk10@gmail.com>
2021-02-18 16:59:44 +0700

    commit tugas 1

---
+++ tugas1helloPBO.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 16:42:02 2021
+
+@author: LENOVO
+"""
+
+print("hello world")
+print("belajar PBO menggunakan bahasa pemrograman Python")
+print("Prodi Sistem Telekomunikasi UPI Kampus di Purwakarta")
